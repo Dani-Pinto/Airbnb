6aa2d2c403c97c7e573bb6f7091884a4a87368bc
Daniel Pinto <danielpinto.ml@gmail.com>
2023-04-23 12:25:15 -0500

    First commit, base project

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Airbnb</title>
+    <link rel="stylesheet" href="build/css/app.css">
+</head>
+<body>
+    <header class="header">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a href="/">Air<span>bnb</span></a>
+
+                <nav class="navegacion">
+                    <a href="">Registrar Vendedor</a>
+                    <a href="">Registrar Apartamento</a>
+                </nav>
+            </div>
+
+            <h1>Arrienda tus Apartamentos</h1>
+        </div>
+    </header>
+</body>
+</html>
